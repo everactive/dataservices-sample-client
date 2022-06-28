321e630d6fc6ea0910fc4b152eced3d3f4704ad6
Austin Schaffer <austin.schaffer@everactive.com>
2022-06-28 16:14:19 -0400

    Added gateway status support to repo layer

--- webhook/src/repository.js
+++ webhook/src/repository.js
@@ -1,31 +1,69 @@
-const { Pool } = require('pg');
+const { Pool } = require("pg");
 
 const pool = new Pool();
 
 var insertReading = async function (reading) {
-    if (!reading) {
-        return;
-    }
-    const macAddress = reading.macAddress;
-    const timestamp = new Date(reading.timestamp * 1000);
-    const serialNumber = reading.gatewaySerialNumber;
-    const schema = reading.schema;
-    const schemaVersion = reading.schemaVersion;
+  if (!reading) {
+    return;
+  }
 
-    const query = `insert into public.sensor_readings (
+  const macAddress = reading.macAddress;
+  const timestamp = new Date(reading.timestamp * 1000);
+  const serialNumber = reading.gatewaySerialNumber;
+  const schema = reading.schema;
+  const schemaVersion = reading.schemaVersion;
+
+  const query = `INSERT INTO public.sensor_readings (
         sensor_mac_address,
         ts,
         gateway_serial,
         schema,
         schema_version,
         reading
-    ) values (
+    ) VALUES (
         $1, $2, $3, $4, $5, $6
     ) ON CONFLICT DO NOTHING`;
-    const values = [macAddress, timestamp, serialNumber, schema, schemaVersion, JSON.stringify(reading)]
-    await pool.query(query, values);
-}
+
+  const values = [
+    macAddress,
+    timestamp,
+    serialNumber,
+    schema,
+    schemaVersion,
+    JSON.stringify(reading),
+  ];
+  
+  await pool.query(query, values);
+};
+
+var insertGatewayStatus = async function (gatewayStatus) {
+  if (!gatewayStatus) {
+    return;
+  }
+
+  const serial = gatewayStatus.serial;
+  const timestamp = new Date(gatewayStatus.timestamp) * 1000;
+  const schema = gatewayStatus.schema;
+
+  const query = `INSERT INTO public.gateway_status (
+        gateway_serial,
+        ts,
+        schema,
+        payload
+    ) VALUES (
+        $1, $2, $3, $4
+    ) ON CONFLICT DO NOTHING`;
+
+  const values = [
+    serial,
+    timestamp,
+    schema,
+    JSON.stringify(gatewayStatus),
+  ];
+
+  await pool.query(query, values);
+};
 
 module.exports = {
-    insertReading: insertReading
+  insertReading: insertReading,
 };
