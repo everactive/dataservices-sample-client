44d08388cd1c853009f8e69d6d84da7e61b44844
Austin T Schaffer <schaffer.austin.t@gmail.com>
2022-06-29 13:23:18 -0400

    Add support for gateway status messages (#3)
    * Added anonymized gateway status message
    
    * Added table to sql init script for gateway status messages
    
    * Inline comments to delineate code blocks
    
    * Error no longer occurs
    
    * Added gateway status support to repo layer
    
    * Added sensor reading to sample data
    
    * Added new API route for handling gateway status messages
    
    * Added new endpoint to documentation
    
    * - vs _
    
    * Bug fixes
    
    * Added gateway status messages to grafana dashboard
    
    * Updated readme to include gateway status messages, including a super cool screenshot
    
    * improved grammar
    
    Co-authored-by: Austin Schaffer <austin.schaffer@everactive.com>

--- webhook/src/app.js
+++ webhook/src/app.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const { v4: uuidv4 } = require('uuid');
-
-var InsertReading = require('./repository').insertReading;
+const {
+    insertReading: InsertReading,
+    insertGatewayStatus: InsertGatewayStatus,
+} = require('./repository');
 
 // Use environment var or create a UUID to act as a "key" to prevent
 // unauthorized POST / GET requests. To set to a known value
@@ -16,16 +18,16 @@ const values = [];
 
 /**
  * GET /health
- * 
+ *
  * Use to determine if the webhook server is up and running.
  */
 app.get('/health', async (req, res) => {
     res.send();
-})
+});
 
 /**
  * GET /
- * 
+ *
  * Return an array the last ten messages received via the POST / endpoint.
  */
 app.get('/', async (req, res) => {
@@ -35,13 +37,13 @@ app.get('/', async (req, res) => {
         return;
     }
     res.send(values);
-})
+});
 
 /**
  * POST /
- * 
+ *
  * Store the body in the ephemeral values array and persist the data
- * to the database.
+ * to the sensor readings table in the database.
  */
 app.post('/', async (req, res) => {
     const reqSecret = req.header('x-api-key');
@@ -56,19 +58,9 @@ app.post('/', async (req, res) => {
     try {
         await InsertReading(doc);
     } catch (e) {
-        // It's possible for webhook publishers to send duplicate
-        // entries to webhook subscribers in some circumstances. This
-        // try/catch is to prevent duplicate key violations from
-        // crashing the webhook receiver.
-        if (e?.constraint?.includes('sensor_readings_pkey')) {
-            console.log(`Duplicate entry received.`);
-            res.sendStatus(304);
-            return;
-        } else {
-            console.log(`Error while saving record: ${JSON.stringify(e)}`)
-            res.sendStatus(400);
-            return;
-        }
+        console.log(`Error while saving record: ${JSON.stringify(e)}`);
+        res.sendStatus(400);
+        return;
     }
 
     values.unshift(doc);
@@ -77,8 +69,40 @@ app.post('/', async (req, res) => {
     }
 
     res.sendStatus(200);
-})
+});
+
+/**
+ * POST /gateway_status
+ *
+ * Store the body in the ephemeral values array and persist the data
+ * to the gateway status table in the database.
+ */
+app.post('/gateway_status', async (req, res) => {
+    const reqSecret = req.header('x-api-key');
+    if (reqSecret !== secret) {
+        res.sendStatus(401);
+        return;
+    }
+
+    const payload = req.body;
+    console.log(`Received payload: ${JSON.stringify(payload)}`);
+
+    try {
+        await InsertGatewayStatus(payload);
+    } catch (e) {
+        console.log(`Error while saving record: ${JSON.stringify(e)}`);
+        res.sendStatus(400);
+        return;
+    }
+
+    values.unshift(payload);
+    if (values.length > 10) {
+        values.length = 10;
+    }
+
+    res.sendStatus(200);
+});
 
 app.listen(port, () => {
     console.log(`listening on port ${port}, api-secret: ${secret}`);
-})
+});
